022ec95fdfbb25fa99ab10093be59301c428dd33
shiro46mt <39456646+shiro46mt@users.noreply.github.com>
2023-05-16 23:46:03 +0900

    add service worker

---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = 'A-Puzzle-A-Day';
+var urlsToCache = [
+    'index.html',
+    'style.css',
+    'main.css',
+    'style.js',
+    'main.js',
+    'solve.js',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
